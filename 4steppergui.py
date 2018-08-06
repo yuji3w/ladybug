194e3d94caf72b692e90d1d287455712647e5399
Yujie Wang <wangyuji431@gmail.com>
2018-08-06 18:11:32 +0000

    updated gui and interface

--- 4steppergui.py
+++ 4steppergui.py
@@ -138,10 +138,11 @@ This one uses the original 2d raster scan from stack.
     YCoord = XYCoord[1]
     ZCoord = list(arange(ZMin, ZMax+1, ZSteps)) #again add one because people often round and you want at least 1
     
-    filetype = ".jpg"
+    filetype = ".png"
     num_pictures = len(XCoord)*len(ZCoord)*NumberOfRotations
     
     StepsPerImage = int(160/NumberOfRotations)
+    print("Stepping {} per image".format(str(StepsPerImage))) #just for debugging
     
     #generate Z positions
     
@@ -210,8 +211,8 @@ This one uses the original 2d raster scan from stack.
             
     
             
-            
-        MoveR(RFORWARD,StepsPerImage,SLOW)
+        if NumberOfRotations > 1:   
+            MoveR(RFORWARD,StepsPerImage,SLOW)
         
     #QuitCamera()
     print ('scan completed successfully after {} seconds! {} images taken'.format(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)), str(num_pictures)))
@@ -598,11 +599,11 @@ def MoveZUpSmall():
 
 def MoveRCWSmall():
     #cw when staring down at spindle or object
-    MoveR(RFORWARD, 10, SLOW)
+    MoveR(RFORWARD, 8, SLOW)
     print("You rotated something clockwise a bit!")
     
 def MoveRCCWSmall(): #counterclockwise
-    MoveR(RBACKWARD, 10, SLOW)
+    MoveR(RBACKWARD, 8, SLOW)
     print("You rotated something counterclockwise a bit!")
     
 def exitProgram():
@@ -659,6 +660,181 @@ def SetYStep():
     YScanStep = GlobalY
     print("Step size for Y has been set to {}".format(YScanStep))
 
+#Begin disgusting block of instructions for keypress. Complicated by fact we want to be able to hold Down.
+#Modified from https://stackoverflow.com/questions/12994796/how-can-i-control-keyboard-repeat-delay-in-a-tkinter-root-window
+#main changes are not displaying the Label and also only enabling key control when checkbox is checked (allow_keypress)
+
+
+def LeftStep(*event): #X
+    MoveYForwardSmall()
+    #pyrint('Left')
+
+    if LeftLabel._repeat_on:
+        win.after(LeftLabel._repeat_freq, LeftStep)
+
+def LeftStop(*event):
+    if LeftLabel._repeat_on:
+        LeftLabel._repeat_on = False
+        win.after(LeftLabel._repeat_freq + 1, LeftStop)
+    else:
+        LeftLabel._repeat_on = True
+
+
+def RightStep(*event):
+    MoveYBackSmall()
+
+    if RightLabel._repeat_on:
+        win.after(RightLabel._repeat_freq, RightStep)
+
+def RightStop(*event):
+    if RightLabel._repeat_on:
+        RightLabel._repeat_on = False
+        win.after(RightLabel._repeat_freq + 1, RightStop)
+    else:
+        RightLabel._repeat_on = True
+
+
+def UpStep(*event):
+    MoveXLeftSmall()
+
+    if UpLabel._repeat_on:
+        win.after(UpLabel._repeat_freq, UpStep)
+
+def UpStop(*event):
+    if UpLabel._repeat_on:
+        UpLabel._repeat_on = False
+        win.after(UpLabel._repeat_freq + 1, UpStop)
+    else:
+        UpLabel._repeat_on = True
+
+def DownStep(*event):
+    MoveXRightSmall()
+
+    if DownLabel._repeat_on:
+        win.after(DownLabel._repeat_freq, DownStep)
+
+def DownStop(*event):
+    if DownLabel._repeat_on:
+        DownLabel._repeat_on = False
+        win.after(DownLabel._repeat_freq + 1, DownStop)
+    else:
+        DownLabel._repeat_on = True
+
+def AStep(*event): #note actual lowercase. FOr clock and counterclockwise rotation
+    print('A')
+
+    if ALabel._repeat_on:
+        win.after(ALabel._repeat_freq, AStep)
+
+def AStop(*event):
+    if ALabel._repeat_on:
+        ALabel._repeat_on = False
+        win.after(ALabel._repeat_freq + 1, AStop)
+    else:
+        ALabel._repeat_on = True
+
+def DStep(*event): #note actual lowercase. FOr clock and counterclockwise rotation
+    print('D')
+
+    if DLabel._repeat_on:
+        win.after(DLabel._repeat_freq, DStep)
+
+def DStop(*event):
+    if DLabel._repeat_on:
+        DLabel._repeat_on = False
+        win.after(DLabel._repeat_freq + 1, DStop)
+    else:
+        DLabel._repeat_on = True
+
+def WStep(*event): #note actual lowercase. For Z AXIS W and S
+    print('W')
+
+    if WLabel._repeat_on:
+        win.after(WLabel._repeat_freq, WStep)
+
+def WStop(*event):
+    if WLabel._repeat_on:
+        WLabel._repeat_on = False
+        win.after(WLabel._repeat_freq + 1, WStop)
+    else:
+        WLabel._repeat_on = True
+
+def SStep(*event): #note actual lowercase. For Z AXIS S and S
+    print('S')
+
+    if SLabel._repeat_on:
+        win.after(SLabel._repeat_freq, SStep)
+
+def SStop(*event):
+    if SLabel._repeat_on:
+        SLabel._repeat_on = False
+        win.after(SLabel._repeat_freq + 1, SStop)
+    else:
+        SLabel._repeat_on = True
+
+
+def allow_keypress():
+    #Checks if button is presed, if so, allows keycontrol
+    
+    if keypress_var.get(): #I can't believe this works. Button is pressed
+        
+        
+        global Leftbound
+        global Leftunbound
+        global Rightbound
+        global Rightunbound
+        global Upbound
+        global Upunbound
+        global Downbound
+        global Downunbound
+        global Abound
+        global Aunbound
+        global Dbound
+        global Dunbound
+        global Wbound
+        global Wunbound
+        global Sbound
+        global Sunbound
+        
+        
+        
+        
+        Leftbound = win.bind('<KeyPress-Left>', LeftStep)
+        Leftunbound = win.bind('<KeyRelease-Left>', LeftStop)
+        Rightbound = win.bind('<KeyPress-Right>', RightStep)
+        Rightunbound = win.bind('<KeyRelease-Right>', RightStop)
+        Upbound = win.bind('<KeyPress-Up>', UpStep)
+        Upunbound = win.bind('<KeyRelease-Up>', UpStop)
+        Downbound = win.bind('<KeyPress-Down>', DownStep)
+        Downunbound = win.bind('<KeyRelease-Down>', DownStop)
+        Abound = win.bind('<KeyPress-a>', AStep)
+        Aunbound = win.bind('<KeyRelease-a>', AStop)
+        Dbound = win.bind('<KeyPress-d>', DStep)
+        Dunbound = win.bind('<KeyRelease-d>', DStop)
+        Wbound = win.bind('<KeyPress-w>', WStep)
+        Wunbound = win.bind('<KeyRelease-w>', WStop)
+        Sbound = win.bind('<KeyPress-s>', SStep)
+        Sunbound = win.bind('<KeyRelease-s>', SStop)
+        
+    else:
+        win.unbind('<KeyPress-Left>', Leftbound)
+        win.unbind('<KeyRelease-Left>', Leftunbound)
+        win.unbind('<KeyPress-Right>', Rightbound)
+        win.unbind('<KeyRelease-Right>', Rightunbound)
+        win.unbind('<KeyPress-Up>', Upbound)
+        win.unbind('<KeyRelease-Up>', Upunbound)
+        win.unbind('<KeyPress-Down>', Downbound)
+        win.unbind('<KeyRelease-Down>', Downunbound)
+        win.unbind('<KeyPress-a>', Abound)
+        win.unbind('<KeyRelease-a>', Aunbound)   
+        win.unbind('<KeyPress-d>', Dbound)
+        win.unbind('<KeyRelease-d>', Dunbound)   
+        win.unbind('<KeyPress-w>', Wbound)
+        win.unbind('<KeyRelease-w>', Wunbound)   
+        win.unbind('<KeyPress-s>', Sbound)
+        win.unbind('<KeyRelease-s>', Sunbound)
+
+#BEGIN WHAT GOES ONSCREEN
 
 win.title("Raspberry Pi GUI")
 win.geometry('1400x880')
@@ -791,6 +967,45 @@ RSetButton = tk.Button(SecondaryBottomFrame, text = "Set number of rotations", f
 RSetButton.pack(side = tk.LEFT, padx=5)
 RSetDropdown = tk.OptionMenu(SecondaryBottomFrame, RSetVar, *FactorsOf160)
 RSetDropdown.pack(side = tk.RIGHT, padx=5)
+
+keypress_var = tk.IntVar() #1 if button is pressed
+keypress_button = tk.Checkbutton(SecondaryBottomFrame, text="ENABLE KEYBOARD CONTROLS", variable=keypress_var, command=allow_keypress)
+keypress_button.pack(side = tk.RIGHT)
+
+
+LeftLabel = tk.Label(win)
+LeftLabel._repeat_freq = int(SLOW*1000*10) #holding Down key, milisecond per repeat.. Delay should be how long it actually takes to move
+LeftLabel._repeat_on = True
+
+RightLabel = tk.Label(win)
+RightLabel._repeat_freq = int(SLOW*1000*10) 
+RightLabel._repeat_on = True
+
+UpLabel = tk.Label(win)
+UpLabel._repeat_freq = int(SLOW*1000*10) 
+UpLabel._repeat_on = True
+
+DownLabel = tk.Label(win)
+DownLabel._repeat_freq = int(SLOW*1000*10)  
+DownLabel._repeat_on = True
+
+ALabel = tk.Label(win) #a nd d are rotation
+ALabel._repeat_freq = 10  
+ALabel._repeat_on = True
+
+DLabel = tk.Label(win)
+DLabel._repeat_freq = 10  
+DLabel._repeat_on = True
+
+WLabel = tk.Label(win)
+WLabel._repeat_freq = 10  
+WLabel._repeat_on = True
+
+SLabel = tk.Label(win)
+SLabel._repeat_freq = 10  
+SLabel._repeat_on = True
+
+
 
 
 
---
+++ checkpress.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+master = Tk()
+
+var = IntVar()
+
+c = Checkbutton(master, text="Expand", variable=var)
+c.pack()
+
