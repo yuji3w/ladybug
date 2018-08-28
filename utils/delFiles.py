e2f7068bb67559c2eae51a8a75b29db3359bf8fe
wangyuji431 <wangyuji431@gmail.com>
2018-08-28 12:37:23 -0400

    added main to delFiles

--- utils/delFiles.py
+++ utils/delFiles.py
@@ -5,22 +5,26 @@ Ahron and Yujie
 
 '''
 
+#TODO: get running. Won't work because of permissions. -Yujie
+#get it working programmatically like you did before, Ahron
+
 
 import os
 import glob
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--folder", required=True, help="folder location")
-parser.add_argument("-e", "--extension", required=False, help="file extension without .")
-args = vars(parser.parse_args())
+def main(folder, extension):
+	files = glob.glob(folder)
+	for f in files:
+		os.remove(f)
 
-folder = args["folder"]
-extension = ".png"
-if args["extension"]:
-	extension = "." + args["extension"]
-
-files = glob.glob(folder)
-for f in files:
-	os.remove(f)
-
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input", required=True, help="folder location")
+	parser.add_argument("-e", "--extension", required=False, help="file extension without .")
+	args = vars(parser.parse_args())
+	folder = args["input"]
+	extension = ".png"
+	if args["extension"]:
+		extension = "." + args["extension"]
+	main(folder, extension)
