3e76394ce9a54874e705aeccbb688504e38481d5
wangyuji431 <wangyuji431@gmail.com>
2018-08-28 12:30:33 -0400

    added main to cv stitching

--- utils/imageStitch.py
+++ utils/imageStitch.py
@@ -6,49 +6,42 @@ Yujie
 
 '''
 
-
 import cv2
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", required=True, help="folder input")
-parser.add_argument("-o", "--output", required=True, help="folder output")
-parser.add_argument("-e", "--extension", required=False, help="file extension without .")
-args = vars(parser.parse_args())
+def cvStitch(folder, output, extension = ".png"):
+	stitcher = cv2.createStitcher(False)
 
-folder = args["input"]
-output = args["output"]
-extension = ".png"
-if args["extension"]:
-	extension = "." + args["extension"]
+	fileList = []
+	imgList = []
 
-stitcher = cv2.createStitcher(False)
+	for file in os.listdir(folder):
+			if file.endswith(extension):
+				fileList.append(os.path.join(folder,file))
 
+	for file in fileList:
+		img = cv2.imread(file)
+		imgList.append(img)
 
-#img1 = cv2.imread(r"C:\Users\wangy\Desktop\Sz0v9.png")
-#img2 = cv2.imread(r"C:\Users\wangy\Desktop\zGwTt.png")
+	imgTuple = tuple(imgList)
+	stitched = stitcher.stitch(imgList)
+	cv2.imwrite(os.path.join(output,os.path.basename(fileList[0])),stitched[1])
 
-
-#stitchedtemp = stitcher.stitch((img1,img2))
-#cv2.imwrite(r"C:\Users\wangy\Desktop\stitchemoutput\whatevs.jpg",stitchedtemp[1])
-
+def main(folder, output, extension):
+	cvStitch(folder, output, extension)
 
 
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input", required=True, help="folder input")
+	parser.add_argument("-o", "--output", required=True, help="folder output")
+	parser.add_argument("-e", "--extension", required=False, help="file extension without .")
+	args = vars(parser.parse_args())
 
-fileList = []
-imgList = []
-
-for file in os.listdir(folder):
-		if file.endswith(extension):
-			fileList.append(os.path.join(folder,file))
-
-for file in fileList:
-	img = cv2.imread(file)
-	imgList.append(img)
-
-imgTuple = tuple(imgList)
-
-stitched = stitcher.stitch(imgList)
-
-cv2.imwrite(os.path.join(output,os.path.basename(fileList[0])),stitched[1])
+	folder = args["input"]
+	output = args["output"]
+	extension = ".png"
+	if args["extension"]:
+		extension = "." + args["extension"]
+	main(folder, output, extension)
