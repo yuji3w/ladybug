8304f25cdae4a8946ea9c1e61a57575940242077
waynewaynehello <ahronwayne@gmail.com>
2021-01-09 21:46:05 -0500

    fixed bug where it wouldn't return best focused image if only one was found. Also make it accept a dictionary of prior focus locations for easier repeated calling.

--- utils/RemoveBlurry.py
+++ utils/RemoveBlurry.py
@@ -6,12 +6,13 @@
 Finds the focus metric which is inversely correlated with blurriness. 
 The most in focus files will have high focus metrics. evalBlur exports a list of sorted tuples from least to most in focus.
 
-Yujie  
+Yujie then corrupted by Ahron years later
 
 '''
 
 from imutils import paths
 import os
+import re
 import os.path
 import cv2
 import argparse
@@ -53,8 +54,9 @@ def evalBlur(folder, AcceptableBlur=200):
         bestImage = ''
 
         # make the image grayscale and use LaPlace to evaluate focus
-
         for imagePath in imagePaths:
+            
+            
             image = cv2.imread(imagePath)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -62,7 +64,72 @@ def evalBlur(folder, AcceptableBlur=200):
 
             focusMetric = variance_of_laplacian(gray)
             blurDict[imagePath] = focusMetric
-            if focusMetric > 0:
+            
+            if focusMetric > bestFocusMetric:
+                bestImage = imagePath
+                bestFocusMetric = focusMetric
+            if focusMetric < AcceptableBlur:  # modification starts here
+                FolderBadImages.append(imagePath)
+
+            if len(FolderBadImages) == len(imagePaths) \
+                and len(FolderBadImages) > 0:  # all fail metric, keep best
+
+                FolderBadImages.remove(bestImage)  # don't delete the best crappy on
+
+        for image in FolderBadImages:
+            BadImages.append(image)
+
+    BadImages = [valid for valid in BadImages if valid != '']
+
+    return BadImages
+
+def MakePositionsFromName(name):
+    #inverse of Make Name From Positions (harder)
+    #from file name or basename returns an (X,Y,Z,R) tuple
+    basename = os.path.splitext(os.path.basename(name))[0]
+    splitname = re.sub( r"([A-Z])", r" \1", basename).split()
+    numbersonly = [x[1:] for x in splitname]
+    positions = [float((x[0:-2] + "." + x[-2:])) for x in numbersonly]
+    positions = tuple(positions)
+
+    return positions
+
+def sortBlur(folder, AcceptableBlur=200, FocusDictionary = {}):
+    #like RemoveBlur but uses passed in dictionary of focuses
+    
+    blurDict = {}
+    bestImages = []
+    BadImages = []  # holds files to delete
+
+    rawFolders = [x[0] for x in os.walk(folder)]
+    for folder in rawFolders:
+        FolderBadImages = []
+        imagePaths = [os.path.join(folder, file) for file in
+                      listdir(folder) if isfile(os.path.join(folder,
+                      file))]
+
+        # print(imagePaths)
+
+        len(imagePaths)
+        bestFocusMetric = 0
+        bestImage = ''
+
+        # make the image grayscale and use LaPlace to evaluate focus
+        for imagePath in imagePaths:
+            
+            
+            #image = cv2.imread(imagePath)
+            #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+                    # for focusMetric, higher is better
+            imagekey = MakePositionsFromName(imagePath)
+            
+            focusMetric = FocusDictionary[imagekey]
+
+            #focusMetric = variance_of_laplacian(gray)
+            blurDict[imagePath] = focusMetric
+            
+            if focusMetric > bestFocusMetric:
                 bestImage = imagePath
                 bestFocusMetric = focusMetric
             if focusMetric < AcceptableBlur:  # modification starts here
@@ -81,11 +148,16 @@ def evalBlur(folder, AcceptableBlur=200):
     return BadImages
 
 
-def main(folder, AcceptableBlur=200, extension='.jpg'):
-    BadImages = evalBlur(folder, AcceptableBlur)
-    print('\n' * 10)
 
-    pprint.pprint(BadImages)
+def main(folder, AcceptableBlur=200, extension='.jpg', FocusDictionary = {}):
+
+    if not FocusDictionary:
+        BadImages = evalBlur(folder, AcceptableBlur)
+    else: #Ahron amended quickly: if locations of focuses passed in, don't recalculate
+        BadImages = sortBlur(folder, AcceptableBlur, FocusDictionary=FocusDictionary)
+    print('\n' * 5)
+
+    #pprint.pprint(BadImages)
     print ('Bad images. Quantity: {}'.format(len(BadImages)))
 
     for image in BadImages:
